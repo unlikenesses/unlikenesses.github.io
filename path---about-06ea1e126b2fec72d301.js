a6f8e117ac5952965f78c29d034d3d6aee512c2e
Alex Pestell <alex.pestell@gmail.com>
2018-02-21 16:30:27 +0000

    Updates

--- path---about-06ea1e126b2fec72d301.js
+++ path---about-06ea1e126b2fec72d301.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xf927f8900006],{376:function(t,e){t.exports={data:{markdownRemark:{html:"<p>A stack-is-half-full developer's reminders, notes-to-self and cod-tutorials.</p>\n<p>You can contact me at unlikenesses [at] gmail.com</p>",fields:{title:"About"}}},pathContext:{slug:"/about/",_PARENT:"SOURCE"}}}});
+webpackJsonp([0xf927f8900006],{360:function(t,e){t.exports={data:{markdownRemark:{html:"<p>A stack-is-half-full developer's reminders, notes-to-self and cod-tutorials.</p>\n<p>You can contact me at unlikenesses [at] gmail.com</p>",fields:{title:"About"}}},pathContext:{slug:"/about/",_PARENT:"SOURCE"}}}});
 //# sourceMappingURL=path---about-06ea1e126b2fec72d301.js.map
